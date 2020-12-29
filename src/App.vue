894039ec2fa2d46c149cd34221b3c22bdf820039
NorthernFish <838399792@qq.com>
2020-12-29 09:36:43 +0800

    添加 axios 请求组件

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,23 @@
 <template>
   <div id="app">
     <h1>鼎红电气</h1>
+    <el-row>
+  <el-button>默认按钮</el-button>
+  <el-button type="primary">主要按钮</el-button>
+  <el-button type="success">成功按钮</el-button>
+  <el-button type="info">信息按钮</el-button>
+  <el-button type="warning">警告按钮</el-button>
+  <el-button type="danger">危险按钮</el-button>
+</el-row>
+
+<el-row>
+  <el-button plain>朴素按钮</el-button>
+  <el-button type="primary" round>主要按钮</el-button>
+  <el-button icon="el-icon-search" circle></el-button>
+  <el-button type="info" plain>信息按钮</el-button>
+  <el-button type="warning" plain>警告按钮</el-button>
+  <el-button type="danger" plain>危险按钮</el-button>
+</el-row>
     <!-- 路由出口 -->
     <router-view/>
   </div>
